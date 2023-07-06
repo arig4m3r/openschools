17e77a144c5375927ec6c49d196c1576402f5973
Ari Rocha <arizinhorocha3@gmail.com>
2023-07-05 23:49:03 -0300

    Removed inter and unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,5 @@
-import Script from "next/script";
 import "./globals.css";
-import { Inter } from "next/font/google";
 import { Providers } from "./providers";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "OpenSchools",
@@ -17,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body>
         <Providers>{children}</Providers>
       </body>
     </html>
